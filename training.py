edd792e101bdab77f070f5037141e98d41449911
gennai <simone.gennai@cern.ch>
2021-05-06 08:07:44 +0200

    saving the loss function for the training steps

--- training.py
+++ training.py
@@ -32,9 +32,9 @@ wpdSM = pd.DataFrame.from_dict(wSM)
 nEntries = 1000000
 npd = npd.head(nEntries*2)
 #to be done for all the pt and mass and met variables
-for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
-       'ptl2', 'ptll']:
-    npd[vars] = npd[vars].apply(numpy.log10)
+#for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
+#       'ptl2', 'ptll']:
+#    npd[vars] = npd[vars].apply(numpy.log10)
 wpdSM = wpdSM.head(nEntries*2)
 
 X_train, X_test, y_train, y_test = train_test_split(npd, npd, test_size=0.2, random_state=1)
@@ -58,14 +58,15 @@ vae.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),  loss=tf.keras.losses
 
 #vae.fit([X_train, X_train,wx], epochs=100, validation_data=(X_test,X_test),use_multiprocessing=True)
 #vae.fit(X_train,X_train, epochs=30,sample_weight=wx, batch_size = 32)
-vae.fit(X_train,X_train, epochs=2,sample_weight=wx, batch_size = 16)
+hist = vae.fit(X_train,X_train, epochs=200,sample_weight=wx, batch_size = 32)
+numpy.savetxt("loss_training.csv", hist.history["loss"],delimiter=',')
 encoder = LatentSpace(original_dim, 2*original_dim, latent_dim, intermediate_dim)
 
 z = encoder.predict(X_train)
 #print z
 #vae.save('vae_denselayers_4Dim_withWeights')
-tf.keras.models.save_model(encoder,'encoder_6_latentDim_BatchNorm_logVars')
-tf.keras.models.save_model(vae,'vae_denselayers_withWeights_6_latentDim_100epoch_BatchNorm_logVars')
+tf.keras.models.save_model(encoder,'encoder_6_latentDim_BatchNorm')
+tf.keras.models.save_model(vae,'vae_denselayers_withWeights_6_latentDim_100epoch_BatchNorm')
 #encoded_data = encoder.predict(X_test)
 #decoded_data = decoder.predict(encoded_data)
 #results = vae.evaluate(X_test, X_test, batch_size=32,sample_weight=wxtest)
