3c72cbbbb967054f8b36e6d9e53e11c87a52a392
gennai <simone.gennai@cern.ch>
2021-05-24 14:23:14 +0200

    updated using log and cuts

--- test_loadVAE.py
+++ test_loadVAE.py
@@ -38,14 +38,17 @@ class LossPerBatch(tf.keras.callbacks.Callback):
 pd_variables = ['deltaetajj', 'deltaphijj', 'etaj1', 'etaj2', 'etal1', 'etal2',
        'met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
        'ptl2', 'ptll']#,'phij1', 'phij2', 'w']
-df = ROOT.RDataFrame("SSWW_SM","../ntuple_SSWW_SM.root")
-dfBSM = ROOT.RDataFrame("SSWW_cW_QU","../ntuple_SSWW_cW_QU.root")
+
+dfAll = ROOT.RDataFrame("SSWW_SM","../ntuple_SSWW_SM.root")
+df = dfAll.Filter("ptj1 > 30 && ptj2 >30 && deltaetajj>2 && mjj>200")
+dfBSMAll = ROOT.RDataFrame("SSWW_cW_QU","../ntuple_SSWW_cW_QU.root")
+dfBSM = dfBSMAll.Filter("ptj1 > 30 && ptj2 >30 && deltaetajj>2 && mjj>200")
 
 npy = df.AsNumpy(pd_variables)
 npd =pd.DataFrame.from_dict(npy)
-#for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
-#       'ptl2', 'ptll']:
-    #npd[vars] = npd[vars].apply(np.log10)
+for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
+       'ptl2', 'ptll']:
+    npd[vars] = npd[vars].apply(np.log10)
 #print npd
 wSM = df.AsNumpy("w")
 wpdSM = pd.DataFrame.from_dict(wSM)
@@ -54,9 +57,9 @@ wpdSM = pd.DataFrame.from_dict(wSM)
 
 npyBSM = dfBSM.AsNumpy(pd_variables)
 npdBSM =pd.DataFrame.from_dict(npyBSM)
-#for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
-#       'ptl2', 'ptll']:
-    #npdBSM[vars] = npdBSM[vars].apply(np.log10)
+for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
+       'ptl2', 'ptll']:
+    npdBSM[vars] = npdBSM[vars].apply(np.log10)
 #print npd
 npywBSM = dfBSM.AsNumpy("w")
 npdwBSM = pd.DataFrame.from_dict(npywBSM)
@@ -89,18 +92,23 @@ X_train = t.transform(X_train)
 X_test = t.transform(X_test)
 npdBSM = t.transform(npdBSM)
 
-test_dataset = tf.data.Dataset.from_tensor_slices((X_test, X_test))
-test_dataset = test_dataset.batch(1)
+#test_dataset = tf.data.Dataset.from_tensor_slices((X_test, X_test))
+#test_dataset = test_dataset.batch(1)
 mylosses = LossPerBatch()
-model = tf.keras.models.load_model('vae_denselayers_withWeights_6_latentDim_100epoch_BatchNorm')
-#model.evaluate(X_test,X_test,batch_size=1,callbacks=[mylosses],verbose=0,sample_weight=wxtest)
+model = tf.keras.models.load_model('vae_denselayers_withWeights_6D_latentDim_1000epoch_batchsize16_log_eventFiltered')
+model.evaluate(X_test,X_test,batch_size=1,callbacks=[mylosses],verbose=0,sample_weight=wxtest)
+""" 
+#How to select events with large loss
 for step, (x_batch_train, y_batch_train) in enumerate(test_dataset):
     loss_weighted =  model.evaluate(x_batch_train,y_batch_train,verbose=0)*wxtest[step]
     if loss_weighted > 0.00001:
         print x_batch_train
+"""
 
 mylosses_BSM = LossPerBatch()
-model.evaluate(npdBSM, npdBSM, batch_size=1,sample_weight=wBSM,callbacks=[mylosses_BSM],verbose=0)
+
+model.evaluate(npdBSM,npdBSM,batch_size=1,callbacks=[mylosses_BSM],verbose=0,sample_weight=wBSM)
+
 
 myloss = mylosses.eval_loss
 myloss_BSM = mylosses_BSM.eval_loss
@@ -109,13 +117,14 @@ np.savetxt("lossBSM.csv", myloss_BSM,delimiter=',')
 
 #print myloss_BSM
 #myloss_BSM2 = mylosses_BSM2.eval_loss
-ax = plt.figure(figsize=(7,5), dpi=100).add_subplot(111)
+ax = plt.figure(figsize=(7,5), dpi=100, facecolor="w").add_subplot(111)
 ax.xaxis.grid(True, which="major")
 ax.yaxis.grid(True, which="major")
-ax.set_ylim(ymax=2000)
-ax.hist(myloss,bins=1000,range=(0.,0.0005),histtype="step",color="blue",alpha=1.)
-ax.hist(myloss_BSM,bins=1000,range=(0.,0.0005),histtype="step",color="red",alpha=1.)
+ax.set_ylim(ymax=3000)
+ax.hist(myloss,bins=1000,range=(0.,0.0005),histtype="step",color="blue",alpha=1.,linewidth=2, label="SM Loss")
+ax.hist(myloss_BSM,bins=1000,range=(0.,0.0005),histtype="step",color="red",alpha=1.,linewidth=2,label ="BSM Loss")
 #plt.hist(myloss_BSM2,bins=100,range=(0.,0.00015),histtype="step",color="green",alpha=1.)
+plt.legend()
 plt.show()
 ax.patch.set_facecolor("w")
 #fig.patch.set_facecolor("w")
@@ -123,25 +132,27 @@ ax.patch.set_facecolor("w")
 #
 # TO ADD: plot output distributions
 #
-encoder = tf.keras.models.load_model('encoder_6_latentDim_BatchNorm_logVars')
+encoder = tf.keras.models.load_model('encoder_6D_latentDim_1000epoch_batchsize16_log_eventFiltered')
 encodedBSM = encoder.predict(npdBSM)
 encodedTest = encoder.predict(X_test)
 
-fig, ((ax0, ax1,ax2)) = plt.subplots(nrows=3, ncols=1)
+fig, ((ax0, ax1,ax2)) = plt.subplots(nrows=1, ncols=3)
 
-ax0.hist(encodedBSM[0:,0],bins=100,range=(-5.,5.),histtype="step",weights=wBSM,color="red",alpha=1.)
-ax0.hist(encodedBSM[0:,1],bins=100,range=(-5.,5.),histtype="step",weights=wBSM,color="blue",alpha=1.)
-ax0.hist(encodedBSM[0:,2],bins=100,range=(-5.,5.),histtype="step",weights=wBSM,color="green",alpha=1.)
-ax0.hist(encodedBSM[0:,3],bins=100,range=(-5.,5.),histtype="step",weights=wBSM,color="black",alpha=1.)
-ax0.set_title('BSM1')
-ax0.patch.set_facecolor("w")
+ax0.scatter(encodedBSM[0:,0],encodedBSM[0:,1],color="red",alpha=0.2)
+ax0.scatter(encodedTest[0:,0],encodedTest[0:,1],color="blue",alpha=0.2)
+ax1.scatter(encodedBSM[0:,2],encodedBSM[0:,3],color="red",alpha=0.2)
+ax1.scatter(encodedTest[0:,2],encodedTest[0:,3],color="blue",alpha=0.2)
+ax2.scatter(encodedBSM[0:,4],encodedBSM[0:,5],color="red",alpha=0.2)
+ax2.scatter(encodedTest[0:,4],encodedTest[0:,5],color="blue",alpha=0.2)
+#ax0.hist(encodedTest[0:,3],bins=100,range=(-5.,5.),histtype="step",weights=wxtest,color="blue",alpha=1.)
+#ax1.hist(encodedBSM[0:,4],bins=100,range=(-5.,5.),histtype="step",weights=wBSM,color="red",alpha=1.)
+#ax1.hist(encodedTest[0:,4],bins=100,range=(-5.,5.),histtype="step",weights=wxtest,color="blue",alpha=1.)
 fig.patch.set_facecolor("w")
 
-ax2.hist(encodedTest[0:,0],bins=100,range=(-5.,5.),histtype="step",weights=wxtest,color="red",alpha=1.)
-ax2.hist(encodedTest[0:,1],bins=100,range=(-5.,5.),histtype="step",weights=wxtest,color="blue",alpha=1.)
-ax2.hist(encodedTest[0:,2],bins=100,range=(-5.,5.),histtype="step",weights=wxtest,color="green",alpha=1.)
-ax2.hist(encodedTest[0:,3],bins=100,range=(-5.,5.),histtype="step",weights=wxtest,color="black",alpha=1.)
-ax2.set_title("SM")
+#ax2.hist(encodedBSM[0:,5],bins=100,range=(-5.,5.),histtype="step",weights=wBSM,color="red",alpha=1.)
+#ax2.hist(encodedTest[0:,5],bins=100,range=(-5.,5.),histtype="step",weights=wxtest,color="blue",alpha=1.)
+
+#ax2.set_title("SM")
 
 #plt.scatter(encodedBSM2[0:,0],encodedBSM2[0:,1],c="green")
 ax2.patch.set_facecolor("w")
