a755a0f7a0f149166b4540c5bc226e644e316d01
gennai <simone.gennai@cern.ch>
2021-05-27 08:43:55 +0200

    updated the training step removing the weights

--- training.py
+++ training.py
@@ -1,16 +1,17 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-import ROOT
 import sys
 import numpy
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import layers
 
+
 #taking the model
 from VAE_model import *
 
-ROOT.ROOT.EnableImplicitMT()
+import ROOT
+#ROOT.ROOT.EnableImplicitMT()
 
 
 
@@ -22,7 +23,8 @@ ROOT.ROOT.EnableImplicitMT()
 pd_variables = ['deltaetajj', 'deltaphijj', 'etaj1', 'etaj2', 'etal1', 'etal2',
        'met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
        'ptl2', 'ptll']#,'phij1', 'phij2', 'w']
-df = ROOT.RDataFrame("SSWW_SM","../ntuple_SSWW_SM.root")
+dfAll = ROOT.RDataFrame("SSWW_SM","../ntuple_SSWW_SM.root")
+df = dfAll.Filter("ptj1 > 30 && ptj2 >30 && deltaetajj>2 && mjj>200")
 
 
 npy = df.AsNumpy(pd_variables)
@@ -32,9 +34,9 @@ wpdSM = pd.DataFrame.from_dict(wSM)
 nEntries = 1000000
 npd = npd.head(nEntries*2)
 #to be done for all the pt and mass and met variables
-#for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
-#       'ptl2', 'ptll']:
-#    npd[vars] = npd[vars].apply(numpy.log10)
+for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
+       'ptl2', 'ptll']:
+    npd[vars] = npd[vars].apply(numpy.log10)
 wpdSM = wpdSM.head(nEntries*2)
 
 X_train, X_test, y_train, y_test = train_test_split(npd, npd, test_size=0.2, random_state=1)
@@ -50,23 +52,24 @@ X_test = t.transform(X_test)
 
 n_inputs = npd.shape[1]
 original_dim = n_inputs
-latent_dim = 6
+latent_dim = 7
 intermediate_dim = 7
+
 vae = VariationalAutoEncoder(original_dim, 2*original_dim, latent_dim,intermediate_dim)  
 vae.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),  loss=tf.keras.losses.MeanSquaredError())
 
 
-#vae.fit([X_train, X_train,wx], epochs=100, validation_data=(X_test,X_test),use_multiprocessing=True)
-#vae.fit(X_train,X_train, epochs=30,sample_weight=wx, batch_size = 32)
-hist = vae.fit(X_train,X_train, epochs=200,sample_weight=wx, batch_size = 32)
-numpy.savetxt("loss_training.csv", hist.history["loss"],delimiter=',')
+#hist = vae.fit(X_train,X_train, epochs=250,sample_weight=wx, batch_size = 16)
+hist = vae.fit(X_train,X_train, epochs=250, batch_size = 16)
+
 encoder = LatentSpace(original_dim, 2*original_dim, latent_dim, intermediate_dim)
 
 z = encoder.predict(X_train)
 #print z
 #vae.save('vae_denselayers_4Dim_withWeights')
-tf.keras.models.save_model(encoder,'encoder_6_latentDim_BatchNorm')
-tf.keras.models.save_model(vae,'vae_denselayers_withWeights_6_latentDim_100epoch_BatchNorm')
+tf.keras.models.save_model(encoder,'encoder_7D_latentDim_1000epoch_batchsize16_log_eventFiltered')
+tf.keras.models.save_model(vae,'vae_denselayers_withWeights_7D_latentDim_1000epoch_batchsize16_log_eventFiltered')
+numpy.savetxt("loss_training_7D_1000epoch_batchsize16_eventFiltered.csv", hist.history["loss"],delimiter=',')
 #encoded_data = encoder.predict(X_test)
 #decoded_data = decoder.predict(encoded_data)
 #results = vae.evaluate(X_test, X_test, batch_size=32,sample_weight=wxtest)
