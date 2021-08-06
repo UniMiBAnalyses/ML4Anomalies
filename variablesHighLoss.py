90c8e574f1a587be6f3c2ff1329dcf1b8c2c19b4
gennai <simone.gennai@cern.ch>
2021-08-06 15:55:14 +0200

    modified slightly

--- variablesHighLoss.py
+++ variablesHighLoss.py
@@ -18,7 +18,7 @@ def sort_by_absolute(df, index):
 
 
 cW = 0.5 #0.3
-nEntries = 500000
+nEntries = 50000
 
 pd_variables = ['deltaetajj', 'deltaphijj', 'etaj1', 'etaj2', 'etal1', 'etal2',
        'met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
@@ -85,20 +85,22 @@ X_test=t.transform(X_test)
 All_test = t.transform(All_test)
 All_BSM_test = t.transform(All_BSM_test)
 
-model = tf.keras.models.load_model('vae_test_noSampling')
-#output = model.predict(All_BSM_test)
-#rec_err = np.linalg.norm(All_BSM_test - output, axis = 1)
-output = model.predict(X_test)
-rec_err = np.linalg.norm(X_test - output, axis = 1)
+model = tf.keras.models.load_model('vae_denselayers_withWeights_7D_latentDim_500epoch_batchsize16_log_eventFiltered_noWeights')
+output = model.predict(All_BSM_test)
+rec_err = np.linalg.norm(All_BSM_test - output, axis = 1)
+#output = model.predict(X_test)
+#rec_err = np.linalg.norm(X_test - output, axis = 1)
 plt.hist(rec_err)
 plt.show()
 idx = list(rec_err).index(max(rec_err))
 worse_variables = []
 shaptop5features = pd.DataFrame(data = None)
 for idx in range(len(rec_err)):
-       if rec_err[idx] > 0.7:#1.22:
-              df = pd.DataFrame(data = All_BSM_test[idx], index = All.columns, columns = ['reconstruction_loss'])
-              #print(sort_by_absolute(df, idx).T)
+       if rec_err[idx] > 0.45:#1.22:
+       #if rec_err[idx] < 0.7:#1.22:
+              diff = All_BSM_test[idx]-output[idx]
+              df = pd.DataFrame(data = diff, index = All.columns, columns = ['reconstruction_loss'])
+              print(sort_by_absolute(df, idx).T)
               top_5_features = sort_by_absolute(df, idx).iloc[:5,:]              
               print(top_5_features.T)
               worse_variables.append(top_5_features.T.columns.to_numpy())
@@ -107,7 +109,7 @@ for idx in range(len(rec_err)):
 shaptop5features = shaptop5features.T
 for i in range(5):
     print("**** Variable counts in column", i)
-    print(shaptop5features[i].value_counts())
+    print(shaptop5features[i].value_counts(normalize=True))
 
 
 """
