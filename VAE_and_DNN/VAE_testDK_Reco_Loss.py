f823ac8416b9f6edf5b4c4dcc98b00c111d441be
gennai <simone.gennai@cern.ch>
2022-04-22 08:55:52 +0200

    added older version of the VAE as a reference

---
+++ VAE_and_DNN/VAE_testDK_Reco_Loss.py
@@ -0,0 +1,248 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+from tensorflow.python.ops import math_ops
+
+class sampling(layers.Layer):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding the variables."""
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+class Encoder(layers.Layer):
+    """Maps PD variables to a triplet (z_mean, z_log_var, z)."""
+    def __init__(self,               
+               intermediate_dim=50,
+               input_dim=20,
+               half_input=10,
+               latent_dim=5,                                             
+               name='encoder',
+               **kwargs):
+        super(Encoder, self).__init__(name=name, **kwargs)                       
+        self.encoder_layer1 = layers.Dense(intermediate_dim)        
+        self.encoder_active_layer1 = layers.LeakyReLU(name="encoder_leakyrelu_1")
+        self.encoder_layer2 = layers.Dense(input_dim)
+        self.encoder_active_layer2 = layers.LeakyReLU(name="encoder_activ_layer_2")
+        self.encoder_layer3 = layers.Dense(input_dim)
+        self.encoder_active_layer3 = layers.LeakyReLU(name="encoder_activ_layer_3")    
+        self.encoder_layer4 = layers.Dense(input_dim)
+        self.encoder_active_layer4 = layers.LeakyReLU(name="encoder_activ_layer_4")    
+        self.encoder_layer5 = layers.Dense(half_input) 
+        self.encoder_active_layer5 = layers.LeakyReLU(name="encoder_activ_layer_5")
+        self.dense_mean = layers.Dense(latent_dim)
+        self.dense_log_var = layers.Dense(latent_dim)
+        self.sampling = sampling()
+
+    def call(self, inputs):
+        xl1 = self.encoder_layer1(inputs)
+        xl1_active = self.encoder_active_layer1(xl1)        
+        xl2 = self.encoder_layer2(xl1_active)        
+        xl2_active = self.encoder_active_layer2(xl2)
+        xl3 = self.encoder_layer3(xl2_active)        
+        xl3_active = self.encoder_active_layer3(xl3)
+        xl4=self.encoder_layer4(xl3_active)
+        xl4_active=self.encoder_active_layer4(xl4)
+        xl5=self.encoder_layer5(xl4_active)        
+        xl5_active=self.encoder_active_layer5(xl5)
+        z_mean = self.dense_mean(xl5_active)
+        z_log_var = self.dense_log_var(xl5_active)
+        z = self.sampling((z_mean, z_log_var))
+        return z_mean, z_log_var, z
+
+
+class Decoder(layers.Layer):
+    """Converts z, the encoded vector, back into a readable list of variables."""
+
+    def __init__(self,
+           half_input=10,
+           input_dim=20,
+           intermediate_dim=50,
+           original_dim=14,
+           name='decoder',
+               **kwargs):
+        super(Decoder, self).__init__(name=name, **kwargs)
+        self.decoder_layer1 = layers.Dense(half_input)        
+        self.decoder_active_layer1 = layers.LeakyReLU(name="decoder_leakyrelu_1")
+        self.decoder_layer2 = layers.Dense(input_dim)                
+        self.decoder_active_layer2 = layers.LeakyReLU(name="decoder_leakyrelu_2")    
+        self.decoder_layer3 = layers.Dense(input_dim)
+        self.decoder_active_layer3 = layers.LeakyReLU(name="decoder_leakyrelu_3")
+        self.decoder_layer4 = layers.Dense(input_dim)
+        self.decoder_active_layer4 = layers.LeakyReLU(name="decoder_leakyrelu_4")
+        self.decoder_layer5 = layers.Dense(intermediate_dim)
+        self.decoder_active_layer5 = layers.LeakyReLU(name="decoder_leakyrelu_5")        
+        self.dense_output = layers.Dense(original_dim)
+        self.decoder_active_output = layers.LeakyReLU(name="decoder_leakyrelu_output")
+
+    def call(self, inputs):
+        layer1 = self.decoder_layer1(inputs)        
+        active_layer1= self.decoder_active_layer1(layer1)
+        layer2=self.decoder_layer2(active_layer1)        
+        active_layer2=self.decoder_active_layer2(layer2)
+        layer3=self.decoder_layer3(active_layer2)    
+        active_layer3=self.decoder_active_layer3(layer3)
+        layer4=self.decoder_layer4(active_layer3)        
+        active_layer4=self.decoder_active_layer4(layer4)
+        layer5=self.decoder_layer5(active_layer4)        
+        active_layer5=self.decoder_active_layer5(layer5)
+        output =self.dense_output(active_layer5)
+        return self.decoder_active_output(output)
+
+#class recoLoss(layers.Layer):
+#    """Computes the reco loss and return it, not trainable."""
+#
+#    def __init__(self,         
+#           name='recoLoss',
+#            **kwargs):        
+#     super(recoLoss, self).__init__(name=name, **kwargs)
+
+# def build(self, input_shape):
+#     #print(input_shape)
+#     super(recoLoss, self).build(input_shape)
+
+# def call(self, inputs):
+#     original = inputs[0]
+#     reconstructed = inputs[1]
+#     nVar = reconstructed.get_shape().as_list()[1]  
+#     dim_batch =  reconstructed.get_shape().as_list()[0]  
+#     #myLoss = np.linalg.norm(reconstructed - original, axis = 1)/nVar                   
+#     myLoss = math_ops.squared_difference(reconstructed, original)
+#     myLoss = tf.keras.backend.mean(myLoss, axis = -1)
+#     #myLoss = tf.reshape(myLoss, [dim_batch,1])   
+#     #print "\n myLoss ", myLoss        
+#     return myLoss
+
+#def custom_loss(y_true, y_pred):
+# diff = math_ops.squared_difference(y_pred, y_true)  #squared difference
+# loss = K.mean(diff, axis=-1) #mean over last dimension
+# loss = loss / 10.0
+# return loss
+#
+
+class classifier(layers.Layer):
+    """Classifies S and B using the loss."""
+
+    def __init__(self,                      
+           name='classifier',
+               **kwargs):
+        super(classifier, self).__init__(name=name, **kwargs)        
+        self.classifier_layer0 = layers.Dense(50,activation="relu")    #it was 50 before    
+        #self.classifier_layer1 = layers.Dense(50,activation="relu")        
+        #self.classifier_layer2 = layers.Dense(10,activation="relu")        
+        self.classifier_layer3 = layers.Dense(1,activation='sigmoid')        
+        
+    def call(self, inputs):
+        #print "\n Inputs classifier", inputs
+        layer0 = self.classifier_layer0(inputs) 
+        #layer1 = self.classifier_layer1(layer0) 
+        #layer2 = self.classifier_layer2(layer1)         
+        output = self.classifier_layer3(layer0)                 
+        #print "classifier output = ",output
+        return output
+
+class VariationalAutoEncoder(tf.keras.Model):
+    """Combines the encoder and decoder into an end-to-end model for training."""
+
+    def __init__(self,
+               original_dim,               
+               intermediate_dim=50,
+               input_dim=20,
+               half_input=10,
+               latent_dim=5,
+               name='autoencoder',
+               **kwargs):
+        super(VariationalAutoEncoder, self).__init__(name=name, **kwargs)
+        self.original_dim = original_dim
+        self.encoder = Encoder(
+               intermediate_dim=intermediate_dim,
+               input_dim=input_dim,
+               half_input=half_input,
+               latent_dim=latent_dim)
+        self.decoder = Decoder(
+           half_input=half_input,
+           input_dim=input_dim,
+           intermediate_dim=intermediate_dim,
+           original_dim=original_dim)   
+        #self.recoLoss = recoLoss()  
+        self.classifier = classifier()                  
+    def call(self, inputs):
+        # self._set_inputs(inputs)
+        z_mean, z_log_var, z = self.encoder(inputs)
+        reconstructed = self.decoder(z)
+        mse = tf.keras.losses.MeanSquaredError()
+        mseLoss = mse(inputs, reconstructed)*1. #was 1. by deafult        
+        self.add_loss(mseLoss) 
+        # Add KL divergence regularization loss.
+        kl_loss = - 0.5 * tf.reduce_mean(
+            z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1)
+        kl_loss= kl_loss/1000000. # was 1000000.
+        self.add_loss(kl_loss)  
+        recoLoss = math_ops.squared_difference(reconstructed, inputs)
+        recoLoss = tf.keras.backend.mean(recoLoss, axis = -1)
+        dim_batch =  reconstructed.get_shape().as_list()[0]   
+        #print "\n recoLoss = ", recoLoss 
+        #recoLoss = tf.reshape(recoLoss, [dim_batch,1])             
+        recoLoss = tf.expand_dims(recoLoss,-1)
+        #print "\n recoLoss = ", recoLoss 
+        newKLLoss = tf.keras.backend.mean(- 0.5 *(z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1), axis = -1)
+        #print "\n KLLoss = ", newKLLoss
+        #totLoss = tf.stack([recoLoss,newKLLoss],axis=1)
+        #print "\n tot Loss ",totLoss                        
+        myOutput = self.classifier(recoLoss)       
+        #myOutput = self.classifier(totLoss)       
+        #myOutput = self.classifier(z)
+        return myOutput
+
+class LatentSpace(tf.keras.Model):
+    """Combines the encoder and decoder into an end-to-end model for training."""
+    def __init__(self,
+               intermediate_dim=50,
+               input_dim=20,
+               half_input=10,
+               latent_dim=5,  
+               name='latentspace',
+               **kwargs):
+        super(LatentSpace, self).__init__(name=name, **kwargs)        
+        self.encoder = Encoder(
+               intermediate_dim=intermediate_dim,
+               input_dim=input_dim,
+               half_input=half_input,
+               latent_dim=latent_dim)
+
+    def call(self, inputs):
+        # self._set_inputs(inputs)
+        z_mean, z_log_var, z = self.encoder(inputs)
+        return z
+
+
+class EncoderDecoder(tf.keras.Model):
+    """Combines the encoder and decoder into an end-to-end model for training."""
+
+    def __init__(self,
+               original_dim,               
+               intermediate_dim=50,
+               input_dim=20,
+               half_input=10,
+               latent_dim=5,
+               name='autoencoder',
+               **kwargs):
+        super(EncoderDecoder, self).__init__(name=name, **kwargs)
+        self.original_dim = original_dim
+        self.encoder = Encoder(
+               intermediate_dim=intermediate_dim,
+               input_dim=input_dim,
+               half_input=half_input,
+               latent_dim=latent_dim)
+        self.decoder = Decoder(
+           half_input=half_input,
+           input_dim=input_dim,
+           intermediate_dim=intermediate_dim,
+           original_dim=original_dim)                        
+    def call(self, inputs):
+        # self._set_inputs(inputs)
+        z_mean, z_log_var, z = self.encoder(inputs)
+        reconstructed = self.decoder(z)
+        return reconstructed      
---
+++ VAE_and_DNN/training_VAE_DNN_Simone.py
@@ -0,0 +1,131 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+import sys
+import numpy
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+#taking the model
+#from VAE_model_extended_moreDKL import *
+from VAE_testDK_Reco_Loss import *
+from matplotlib import pyplot as plt
+
+import ROOT
+#ROOT.ROOT.EnableImplicitMT()
+
+
+
+
+#
+# variable from the nutple
+#
+pd_variables = ['deltaetajj', 'deltaphijj', 'etaj1', 'etaj2', 'etal1', 'etal2',
+       'met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
+       'ptl2', 'ptll']#,'phij1', 'phij2', 'w']
+kinematicFilter = "ptj1 > 30 && ptj2 >30 && deltaetajj>2 && mjj>200"
+dfSM = ROOT.RDataFrame("SSWW_SM","../ntuple_SSWW_SM.root")
+dfSM = dfSM.Filter(kinematicFilter)
+dfBSM = ROOT.RDataFrame("SSWW_cW_QU","../ntuple_SSWW_cW_QU.root")
+dfBSM = dfBSM.Filter(kinematicFilter)
+
+np_SM = dfSM.AsNumpy(pd_variables)
+wSM = dfSM.AsNumpy(["w"])
+npd =pd.DataFrame.from_dict(np_SM)
+wpdSM = pd.DataFrame.from_dict(wSM)
+
+np_BSM = dfBSM.AsNumpy(pd_variables)
+wBSM = dfBSM.AsNumpy(["w"])
+npd_BSM =pd.DataFrame.from_dict(np_BSM)
+wpdBSM = pd.DataFrame.from_dict(wBSM)
+
+nEntries = 300000
+npd = npd.head(nEntries)
+npd_BSM = npd_BSM.head(nEntries)
+wpdSM = wpdSM.head(nEntries)
+wpdBSM = wpdBSM.head(nEntries)
+#to be done for all the pt and mass and met variables
+for vars in ['met', 'mjj', 'mll',  'ptj1', 'ptj2', 'ptl1',
+       'ptl2', 'ptll']:
+    npd[vars] = npd[vars].apply(numpy.log10)
+    npd_BSM[vars] = npd_BSM[vars].apply(numpy.log10)
+
+Y_true = np.full(nEntries,0)
+Y_true_BSM = np.full(nEntries,1)
+#concatenating SM and BSM
+samples = np.concatenate((npd,npd_BSM))
+labels = np.concatenate((Y_true,Y_true_BSM))
+
+X_train, X_test, y_train, y_test = train_test_split(samples, labels, test_size=0.2, random_state=1)
+SM_train,SM_test,_,_ = train_test_split(npd, npd, test_size=0.2, random_state=1)
+BSM_train,BSM_test,_,_ = train_test_split(npd_BSM, npd_BSM, test_size=0.2, random_state=1)
+#wx_train, wx_test, wy_train, wy_test = train_test_split(wpdSM, wpdSM, test_size=0.2, random_state=1)
+
+#BSM_train, BSM_test, y_BSM_train, y_BSM_test = train_test_split(npd_BSM, Y_true_BSM, test_size=0.2, random_state=1)
+#wBSM_train, wBSM_test, _ , _ = train_test_split(wpdBSM, wpdBSM, test_size=0.2, random_state=1)
+#print wx_train,X_train
+#wx = wx_train["w"].to_numpy()
+#wxtest = wx_test["w"].to_numpy()
+#wBSM = wBSM_train["w"].to_numpy()
+#wBSMtest = wBSM_test["w"].to_numpy()
+# scale data
+
+t = MinMaxScaler()
+#t = StandardScaler()
+t.fit(SM_train)
+X_train = t.transform(X_train)
+X_test = t.transform(X_test)
+BSM_test = t.transform(BSM_test)
+
+n_inputs = npd.shape[1]
+original_dim = n_inputs
+
+intermediate_dim = 50 #50 by default
+input_dim = 10 #was 20 in default
+half_input = 7 #was 20 in the newTest
+latent_dim = 3 #tried 7 as well  #was 3 for optimal performance
+epochs = 100 #80
+batchsize=64 #32
+nameExtenstion = str(intermediate_dim)+"_"+str(input_dim)+"_"+str(half_input)+"_"+str(latent_dim)+"_"+str(epochs)+"_"+str(batchsize)
+vae = VariationalAutoEncoder(original_dim,intermediate_dim,input_dim,half_input,latent_dim)  
+#vae.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),  loss=tf.keras.losses.MeanSquaredError())
+#vae.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),run_eagerly=True, loss="binary_crossentropy",metrics = [tf.keras.metrics.BinaryAccuracy()])
+#vae.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005), loss_weights=[0.1],loss="binary_crossentropy",metrics = [tf.keras.metrics.BinaryAccuracy()])
+vae.compile(optimizer=tf.keras.optimizers.Adam(lr=0.0005),loss="binary_crossentropy",metrics = [tf.keras.metrics.BinaryAccuracy()])
+hist = vae.fit(X_train, y_train,validation_data=(X_test,y_test), epochs=epochs, batch_size = batchsize) 
+#print "new model: ", vae.summary()
+encoderDecoder =  EncoderDecoder(original_dim,intermediate_dim,input_dim,half_input,latent_dim)
+reco = encoderDecoder.predict(X_test)
+#encoder = LatentSpace(intermediate_dim,input_dim,half_input,latent_dim)
+#z = encoder.predict(X_train)
+tf.keras.models.save_model(encoderDecoder,'encoderDecoder_newModelUsingKL_Reco_Loss_newWayToAddUpSamples_'+nameExtenstion)
+tf.keras.models.save_model(vae,'vae_newModelUsingKL_Reco_Loss_newWayToAddUpSamples_'+nameExtenstion)
+#numpy.savetxt("lossVAE_test_newModelDimenstions_MinMaxScaler_"+nameExtenstion+".csv",hist.history["loss"],delimiter=",")
+#vae=tf.keras.models.load_model('vae_test_newModelUsingLatentSpace_'+nameExtenstion)
+
+
+output_SM = vae.predict(X_test)
+output_BSM = vae.predict(BSM_test)
+
+#print output_SM
+#print output_BSM
+bins=100
+ax = plt.figure(figsize=(7,5), dpi=100, facecolor="w").add_subplot(111)
+ax.xaxis.grid(True, which="major")
+ax.yaxis.grid(True, which="major")
+ax.hist(output_SM,bins=bins, density=1,range=[0.,1.],histtype="step",color="red",alpha=0.6,linewidth=2,label="SM Output")                        
+ax.hist(output_BSM,bins=bins, density=1,range=[0.,1.],histtype="step",color="blue",alpha=0.6,linewidth=2,label="BSM Output")                        
+plt.rc('legend',fontsize='small')    
+plt.show()
+
+
+x_bins = range(len(hist.history["loss"]))
+ax = plt.figure(figsize=(7,5), dpi=100, facecolor="w").add_subplot(111)
+ax.xaxis.grid(True, which="major")
+ax.yaxis.grid(True, which="major")
+ax.plot(x_bins,hist.history["loss"],color="blue",label="training losss")                        
+plt.rc('legend',fontsize='small')    
+plt.show()
